f4694ef6647cb3966acbe9eeb08ac2d95ffdd303
sinamong0620 <sinaskim60@gmail.com>
2023-07-28 23:55:33 +0900

    Modal component style complete

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,4 +1,32 @@
+import React from "react";
+import styles from "./Modal.module.css";
+
 const Modal = () => {
-  return <></>;
+  return (
+    <div className={styles.ModalContainer}>
+      <div className={styles.ModalYourCartContainer}>
+        <div className={styles.ModalYourCart}>
+          <div className={styles.ModalMyProduct}>
+            <div>
+              <div>Schnitzel</div>
+              <div>$16.5</div>
+            </div>
+            <div className={styles.ModalMyProductCount}>x2</div>
+          </div>
+          <div>
+            <button className={styles.ModalMyProjuctCountButton}>-</button>
+            <button className={styles.ModalMyProjuctCountButton}>+</button>
+          </div>
+        </div>
+        <hr className={styles.ModalHrLine}></hr>
+        <div className={styles.ModalTotalContainer}>
+          <div>Total Amount</div>
+          <div>{`$${"33.00"}`}</div>
+        </div>
+        <button className={styles.ModalCloseButton}>close</button>
+        <button>order</button>
+      </div>
+    </div>
+  );
 };
 export default Modal;
